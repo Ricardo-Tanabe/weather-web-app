5a9bd03e7e3d46f8d7ad9aa3aba13cc8d13e0eae
Ricardo-Tanabe <uhtanabe@gmail.com>
2025-01-16 16:56:25 -0300

    Teste com os dados do site visual crossing

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
-const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-
 async function getWeather() {
-    const location = 'São Luís';
+    const location = 'SãoLuís,MA,Brasil';
     const date1 = '2025-01-15';
-    const date2 = '2025-01-14';
+    const date2 = '2025-01-16';
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
     try {
         const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=${apiKey}`;
         const response = await fetch(url);
@@ -15,7 +13,10 @@ async function getWeather() {
         if(!data) {
             throw new Error('Invalid data format');
         }
-        console.log(data);
+        const previousDay = data.days[0];
+        const currentDay = data.days[1];
+        console.log(previousDay);
+        console.log(currentDay);
     } catch(error) {
         console.error(error);
     }
